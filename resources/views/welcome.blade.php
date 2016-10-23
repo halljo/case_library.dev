1c1f1b4ec553e49369ba1c8cc93bdb75ef9598a2
halljo <johnnydhall@gmail.com>
2016-10-23 11:58:37 +0000

    testing initial syncing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - This is now using git!!
                 </div>
 
                 <div class="links">
